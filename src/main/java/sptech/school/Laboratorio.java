360a3200457d8a0c9de9b5666e4b4e4971b6553f
Gabi476 <gabriele.dutra@sptech.school>
2025-10-22 14:16:00 -0300

    Gabriele Leandro 04251140

--- src/main/java/sptech/school/Laboratorio.java
+++ src/main/java/sptech/school/Laboratorio.java
@@ -1,5 +1,89 @@
-package school.sptech;
+package sptech.school;
+import school.sptech.exception.VacinaNaoEncontradaException;
+import sptech.school.exception.ArgumentoInvalidoException;
+import sptech.school.exception.VacinaInvalidaException;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Laboratorio {
+    private String nome;
+    private List<Vacina> vacinas;
+
+    public Laboratorio(){
+        this.vacinas = new ArrayList<>();
+    }
+
+    public void adicionarVacina(Vacina vacina){
+        if (vacina == null) {
+            throw new VacinaInvalidaException();
+        } else if (vacina.getCodigo() == null || vacina.getCodigo().isEmpty()) {
+            throw new VacinaInvalidaException();
+        } else if (vacina.getNome() == null || vacina.getNome().isEmpty()) {
+            throw new VacinaInvalidaException();
+        } else if (vacina.getTipo() == null || vacina.getTipo().isEmpty()) {
+            throw new VacinaInvalidaException();
+        } else if (vacina.getPreco() == null || vacina.getPreco() < 0) {
+            throw new VacinaInvalidaException();
+        } else if (vacina.getEficacia() < 0 && vacina.getEficacia() > 5) {
+            throw new VacinaInvalidaException();
+        } else if (vacina.getDataLancamento() == null) {
+            throw new VacinaInvalidaException();
+        } else {
+            vacinas.add(vacina);
+        }
+    }
+
+    public void buscarVacinaPorCodigo(String codigo) {
+        for(int i=0; i<vacinas.size(); i++) {
+            if (codigo == null || codigo.isEmpty() || codigo.isBlank()) {
+                throw new ArgumentoInvalidoException();
+            }else if(!codigo.equals(vacinas.get(i).getCodigo())){
+                throw new VacinaNaoEncontradaException();
+            }else{
+                vacinas.get(i).getCodigo();
+            }
+        }
+    }
+
+    public void removerVacinaPorCodigo(String codigo){
+        for(int i=0; i<vacinas.size(); i++){
+            if(codigo == null || codigo.isEmpty()|| codigo.isBlank() ){
+                throw new ArgumentoInvalidoException();
+            }else if(!codigo.equals(vacinas.get(i).getCodigo())){
+                throw new VacinaNaoEncontradaException();
+            }else{
+                vacinas.remove(i);
+            }
+        }
+    }
+
+    public void buscarVacinaComMelhorEficacia(){
+        for(int i=0; i<vacinas.size(); i++){
+            if(vacinas.isEmpty()){
+                throw new VacinaNaoEncontradaException();
+            }else{
+               vacinas.get(i);
+            }
+        }
+    }
+
+    public List<Vacina> buscarVacinaPorPeriodo(LocalDate dataInicio, LocalDate dataFim){
+        for(int i = 0; i<vacinas.size(); i++){
+            if(dataInicio == null || dataFim == null){
+                throw new VacinaNaoEncontradaException();
+            }
+        }
+        return vacinas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
 }
--- src/test/java/sptech/school/logica/LaboratorioAdicionarVacinaTest.java
+++ src/test/java/sptech/school/logica/LaboratorioAdicionarVacinaTest.java
@@ -1,4 +1,4 @@
-package school.sptech.logica;
+package sptech.school.logica;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.TestInstance;
